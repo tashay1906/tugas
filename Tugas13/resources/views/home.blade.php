d2a5f022374d6364645239080c32cad0e8e7d90b
tashay1906 <129875766+tashay1906@users.noreply.github.com>
2023-04-13 23:52:00 +0700

    upload tugas 13

---
+++ Tugas13/resources/views/home.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Index Form</title>
+</head>
+<body>
+  <h1>Garuda Cyber Institute </h1>
+  <h2>Jadilah Programmer Handal Bersama GC-INS </h2>
+  <p>Grow Together With Garuda Cyber Institute</p>
+  <h2>Syarat dan Ketentuan</h2>
+  <ul type="disc">
+    <li>Tamatan SMA/SMK</li>
+    <li>Tamatan Perguruan Tinggi</li>
+    <li>Pekerja IT</li>
+    <li>Freelancer</li>
+</ul>
+  <h2>Cara Bergabung</h2>
+  <ol>
+    <li>Kunjungi website GC</li>
+    <li><a href="register">Register</a></li>
+    <li>Lakukan Pembayaran</li>
+</ol>
+</body>
+</html>
---
+++ Tugas13/routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\IndexController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+});
+Route::get('/home', [HomeController::class, 'home']);
+Route::get('/register', [AuthController::class, 'register']);
+Route::post('/welcome', [AuthController::class, 'welcome']);
+
+
+// Route::get('/data-table', function(){
+//     return view('data-table');
+// });
+
+// Route::get('/table', function(){
+//     return view('table');
+// });
+
+// Tugas Templating
+Route::get('/admin', [IndexController::class, 'home']);
+
